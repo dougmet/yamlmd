13dee10e5d87f6c3160743f5b9457d2c6def0af9
Doug Ashton <douglas.j.ashton@gmail.com>
2017-10-10 13:31:16 +0100

    initial read function

---
+++ yamlmd/read.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 12:30:57 2017
+
+@author: dashton
+"""
+# %% Imports
+
+import yaml
+
+# %% Yaml header
+
+
+def read_yaml_header(fname):
+    """Do the docstring"""
+    with open(fname, encoding="UTF-8") as stream:
+        meta = next(yaml.safe_load_all(stream))
+    return meta
+
+# %% Markdown
+
+
+def read_markdown(fname):
+    """Do the docstring"""
+    with open(fname) as f:
+        content = f.readlines()
+    yaml_delim = [i for i, x in enumerate(content) if x.startswith("---")]
+    assert len(yaml_delim) > 1
+
+    start_line = yaml_delim[1]+1
+
+    return content[start_line:]
+
+# %% Read everything
+
+
+def read_yamlmd(fname):
+    """Doug do the docstring"""
+    return [read_yaml_header(fname), read_markdown(fname)]
